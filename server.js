3b597e6deb3aa6da9f902c44134252850a37c885
Breedlove13 <b.breedlove@outlook.com>
2021-04-18 09:13:47 -0700

    inquirer working, need to build out functions

--- server.js
+++ server.js
@@ -13,7 +13,42 @@ function employeeChartNavigation () {
             choices: ['view all departments','view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
         }
 
-    ]).then(choice => )
+    ]).then((choice) => {
+      if (choice.directory === "view all departments"){
+      
+        console.log("showing all departments");
+      //*allDepartments()
+
+       } else if (choice.directory === 'view all roles'){
+
+        console.log('showing all roles')
+        //*allRoles()
+       } else if(choice.directory === 'view all employees'){
+
+        console.log('showing all employees')
+
+        //*allEmployees
+
+       } else if(choice.directory === 'add a department'){
+          console.log('adding to department')
+          
+          //* addADepartment
+       } else if(choice.directory === 'add a role'){
+
+        console.log('adding a role')
+
+        //*addARole
+       } else if (choice.directory === 'add an employee'){
+
+        console.log('adding an employee')
+
+        //*addAnEmployee()
+       } else{
+           console.log("updating employee")
+           //*unpdateAnEmployeeRole
+        }
+
+    })
 
 }
 
