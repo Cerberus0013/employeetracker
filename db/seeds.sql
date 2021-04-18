f660ebdd13cf0a8cdb3fee0ffb5421ca571826d6
Breedlove13 <b.breedlove@outlook.com>
2021-04-17 21:45:35 -0700

    continuing to set up

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+  INSERT INTO employee (first_name, last_name)
+VALUES
+
+
+  INSERT INTO role (name_first, las)
+VALUES
+
+
+  INSERT INTO department (name, description)
+VALUES
--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-const enquirer = require('inquirer')
+const inquirer = require('inquirer')
 
+
+
+function employeeChartNavigation () {
+
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'directory',
+            message: 'Please pick an option',
+            choices: ['view all departments',' view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+        }
+
+    ])
+
+}
+
+
+
+
+employeeChartNavigation()
